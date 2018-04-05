fb5d0f052bb8319d2c147e7ec56df84fdc48d413
tom.park <uzen.tom.park@gmail.com>
2018-04-06 00:58:56 +0900

    Initial commit

---
+++ src/main/java/pro/tompark/swingheil/member/service/MemberService.java
@@ -0,0 +1,8 @@
+package pro.tompark.swingheil.member.service;
+
+import pro.tompark.swingheil.member.model.Member;
+
+public interface MemberService {
+
+    Member createMember(Member member);
+}
---
+++ src/test/java/pro/tompark/swingheil/member/service/MemberServiceTest.java
@@ -0,0 +1,11 @@
+package pro.tompark.swingheil.member.service;
+
+import org.junit.Test;
+
+public class MemberServiceTest {
+
+    @Test
+    public void testCreateMember() {
+        System.out.println("test");
+    }
+}
